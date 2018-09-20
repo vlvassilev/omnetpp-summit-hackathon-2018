6f5892b16a06ecca2dee17d7d156cf42563c1d4c
TÖRÖK Attila <torokati44@gmail.com>
2018-09-20 18:01:40 +0200

    Overhaul the setup script - use OMNeT++ 5.4.1, and run an example translation and simulation as well.

--- setup-hello.sh
+++ setup-hello.sh
@@ -1,32 +1,24 @@
 #!/bin/bash -e
-#Assuming ~/omnetpp-summit-hackathon-2018 repo directory and ~/omnetpp-5.3-src-linux.tgz are in home directory 
-cd
-tar -xzvf omnetpp-5.3-src-linux.tgz # if the OMNeT++ IDE is not needed at all, the "core" release can also be used instead of the "linux" one - it's a smaller archive
-cd omnetpp-5.3
-apt-get -y install build-essential gcc g++ bison flex perl qt5-default tcl-dev tk-dev libxml2-dev zlib1g-dev default-jre doxygen graphviz libwebkitgtk-3.0-0 # the default-jre doxygen graphviz libwebkitgtk-3.0-0 packages are not needed for the "core" release, only for the IDE
-apt-get -y install libopenscenegraph-dev openscenegraph-plugin-osgearth libosgearth-dev
-apt-get -y install openmpi-bin libopenmpi-dev
-apt-get -y install openscenegraph libopenscenegraph-dev openscenegraph-plugin-osgearth libosgearth-dev
-apt-get -y install xinit
-#the configure script invokes a tool 'wish' that expects valid $DISPLAY
-nohup xinit &
-export DISPLAY=:0.0
-sleep 10
-./configure
-export PATH=~/omnetpp-5.3/bin:$PATH
-make
 
-cd ~/omnetpp-summit-hackathon-2018/hello/
-make
-./hello -u Cmdenv -G
+apt-get update
+apt-get -y install build-essential git python python3 python3-pip gcc g++ bison flex perl qt5-default tcl-dev tk-dev libxml2-dev zlib1g-dev qt5-default libqt5opengl5-dev
+
+# Assuming ~/omnetpp-summit-hackathon-2018 repo directory and ~/omnetpp-5.4.1-src-core.tgz are in home directory
+cd
+tar -xzvf omnetpp-5.4.1-src-core.tgz
+cd omnetpp-5.4.1
+./configure WITH_TKENV=no WITH_QTENV=yes WITH_OSG=no WITH_OSGEARTH=no
+export PATH=~/omnetpp-5.4.1/bin:$PATH
+make -j $(nproc)
 
 # Download and build INET 3.6.4
 cd
 git clone --branch v3.6.4 --recursive https://github.com/inet-framework/inet.git
 cd inet
 make makefiles
-make
-# Run an example simulation of INET4, with an ordinary ethernet switch
+make -j $(nproc)
+
+# Run an example simulation of INET, with an ordinary ethernet switch
 cd examples/ethernet/lans/
 ./run duplexswitch.ini -c SwitchedDuplexLAN -u Cmdenv # delete the "-u Cmdenv" part for the graphical interface
 
@@ -35,11 +27,22 @@ cd
 git clone https://gitlab.com/ipvs/nesting.git
 cd nesting
 make makefiles
-make
+make -j $(nproc)
 # Run an example simulation of NeSTiNg
 cd simulations
 ./runsim example.ini                # run simulation without graphical interface (release)
 # ./runsim-qt example.ini                # simulation with the Qt interface (release)
 
+cd
 
-#omnetpp
+pip3 install --user lxml
+
+mkdir example-sim -p
+cd example-sim
+
+cp ~/omnetpp-summit-hackathon-2018/topology-with-config.xml .
+
+~/omnetpp-summit-hackathon-2018/sim_net topology-with-config.xml --sim-time-limit=1s
+
+opp_scavetool export -T s -f 'name(*capPk*)' -w -F CSV-S -o out.csv results/*.sca
+cat out.csv
