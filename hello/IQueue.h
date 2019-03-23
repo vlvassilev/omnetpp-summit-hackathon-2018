4714eeb8109c070ac7bfa606f1d527d8c013530a
TÖRÖK Attila <torokati44@gmail.com>
2019-03-23 19:56:56 +0100

    Repo cleanup.

--- hello/IQueue.h
+++
@@ -1,9 +0,0 @@
-class IQueue
-{
-    public:
-        virtual ~IQueue() { };
-        // the current length of the queue
-        virtual unsigned int length() = 0;
-        // pop a frame.
-        virtual void pop() = 0;
-};
--- hello/libtraffic.h
+++
@@ -1,4 +0,0 @@
-typedef void* traffic_generator_instance_t;
-extern "C" traffic_generator_instance_t traffic_generator_init(const char* config_str);
-extern "C" int traffic_generator_get_frame(traffic_generator_instance_t tg_id, uint32_t* len, uint8_t** data, uint64_t* tx_time_sec, uint32_t* tx_time_nsec);
-
